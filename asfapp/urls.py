294b9623e4139d9ae1be79c867a8786c6a9dd427
adraposo <ad.wilson@hotmail.com>
2024-08-03 14:35:00 -0300

    alteracao solucionar problema no staticfiles

--- asfapp/urls.py
+++ asfapp/urls.py
@@ -1,12 +1,21 @@
 
-from django.urls import path, reverse_lazy
+from django.urls import path, reverse_lazy, re_path                                       #re_path acrecentada para retirar problema debug = fase
 from .views import Homepage, Homecursos, Detalhecurso, Pesquisarcurso, Editarperfil, Criarconta
+from django.conf import settings                                                          #linha acrecentada para retirar problema debug = fase
 from django.contrib.auth import views as auth_view
+from django.views.static import serve                                                     #linha acrecentada para retirar problema debug = fase
+
+from django.contrib import admin                                                          #linha acrecentada para retirar problema debug = fase
+from django.conf.urls.static import static                                                #linha acrecentada para retirar problema debug = fase
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns                       # #linha acrecentada para retirar problema debug = fasenew
+
 
 app_name = 'curso'
 
 urlpatterns = [
 
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),      #linha acrecentada para retirar problema debug = fase
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),    #linha acrecentada para retirar problema debug = fase
     path('', Homepage.as_view(), name='homepage'),
     path('cursos/', Homecursos.as_view(), name='homecursos'),
     path('cursos/<int:pk>', Detalhecurso.as_view(), name='detalhecurso'),
